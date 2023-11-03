c28dbb75ca8c7eecf94c6d61a7a75f811406860a
zikalop <vojtazika2@gmail.com>
2023-11-04 00:13:24 +0100

    Přidejte soubory projektu.

---
+++ WPF2/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace WPF2
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
